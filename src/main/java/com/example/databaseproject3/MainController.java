01f4ee4d09eec313d4da175df164daa81fac66ae
stella-craig <stellarstellacraig@gmail.com>
2023-05-05 18:45:31 -0500

    -

--- src/main/java/com/example/databaseproject3/MainController.java
+++ src/main/java/com/example/databaseproject3/MainController.java
@@ -1,4 +1,14 @@
 package com.example.databaseproject3;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+
 public class MainController {
+
+
+    @FXML
+    protected void onExitButtonClick()
+    {
+        Platform.exit();
+    }
 }
