6bee8d6b05fa1bb9e482caa5b17edb0485647957
stella-craig <stellarstellacraig@gmail.com>
2023-05-05 22:26:36 -0500

    -

--- src/main/java/com/example/databaseproject3/Functions_for_BReturnBook.java
+++ src/main/java/com/example/databaseproject3/Functions_for_BReturnBook.java
@@ -11,4 +11,12 @@ public class Functions_for_BReturnBook {
         return 0;
     }
 
+    public static int booksByISBN(String isbn)
+    {
+        String prompt = "SELECT COUNT(*) as Num_Books FROM BOOK WHERE ISBN = ";
+        prompt = prompt.concat(isbn);
+        prompt = prompt.concat(" AND Curr_Location <> Orig_Location;");
+        return 0;
+    }
+
 }
