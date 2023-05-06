204294cc4ecc60153c17810d29469bca4fd333c5
Andrew <andrew.klusmeyer@gmail.com>
2023-05-05 23:26:30 -0500

    andrews help

--- src/main/java/com/example/databaseproject3/Functions_for_Find_Book_Info.java
+++ src/main/java/com/example/databaseproject3/Functions_for_Find_Book_Info.java
@@ -43,6 +43,7 @@ public class Functions_for_Find_Book_Info {
         String username = "root";
         String password = "Hickman21!";
 
+        Class.forName("com.mysql.cj.jdbc.Driver");
         try (Connection conn = DriverManager.getConnection(url, username, password);
              PreparedStatement stmt = conn.prepareStatement("SELECT * FROM home_library.Book WHERE ISBN = ?")) {
             stmt.setString(1, isbn);
@@ -71,73 +72,99 @@ public class Functions_for_Find_Book_Info {
 
 
     public static void searchByAuthorFName(String fName) throws SQLException {
-
-        String url = "jdbc:mysql://localhost:3306/?useSSL=false";
+        String url = "jdbc:mysql://localhost:3306";
         String username = "root";
         String password = "Hickman21!";
 
-        try {
-           // Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(url, username, password);
-            System.out.println("Connected to the database");
-            // Do something with the connection...
-            Statement stmt = conn.createStatement();
-            String prompt = "SELECT * FROM home_library.book JOIN home_library.author ON book.Author_id = author.Author_id WHERE author.First_Name = ";
-            prompt = prompt.concat(fName);
-            prompt = prompt.concat(";");
-            ResultSet rs = stmt.executeQuery(prompt);
-            conn.close();
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM home_library.book JOIN home_library.author ON book.Author_id = author.Author_id WHERE author.First_Name = ?")) {
+            stmt.setString(1, fName);
+            ResultSet rs = stmt.executeQuery();
+            boolean found = false;
+
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            while (rs.next()) {
+                found = true;
+                // print all columns dynamically
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(metaData.getColumnName(i) + ": " + rs.getString(i) + " ");
+                }
+                System.out.println();
+            }
+            if (!found) {
+                System.out.println("No books found with the author's first name: " + fName);
+            }
         } catch (SQLException e) {
             System.out.println("Unable to connect to the database: " + e.getMessage());
         }
-
     }
 
-    public static void searchByAuthorLName(String lName) throws SQLException {
 
-        String url = "jdbc:mysql://localhost:3306/?useSSL=false";
+    public static void searchByAuthorLName(String lName) throws SQLException, ClassNotFoundException {
+        String url = "jdbc:mysql://localhost:3306";
         String username = "root";
         String password = "Hickman21!";
 
-        try {
-           // Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(url, username, password);
-            System.out.println("Connected to the database");
-            // Do something with the connection...
-            Statement stmt = conn.createStatement();
-            String prompt = "SELECT * FROM home_library.book JOIN home_library.author ON book.Author_id = author.Author_id WHERE author.Last_Name = ";
-            prompt = prompt.concat(lName);
-            prompt = prompt.concat(";");
-            ResultSet rs = stmt.executeQuery(prompt);
-            conn.close();
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM home_library.book JOIN home_library.author ON book.Author_id = author.Author_id WHERE author.Last_Name = ?")) {
+            stmt.setString(1, lName);
+            ResultSet rs = stmt.executeQuery();
+            boolean found = false;
+
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            while (rs.next()) {
+                found = true;
+                // print all columns dynamically
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(metaData.getColumnName(i) + ": " + rs.getString(i) + " ");
+                }
+                System.out.println();
+            }
+            if (!found) {
+                System.out.println("No books found with the author's last name: " + lName);
+            }
         } catch (SQLException e) {
             System.out.println("Unable to connect to the database: " + e.getMessage());
         }
-
     }
+
 
     public static void searchByAuthorGender(String gender) throws SQLException {
-
-        String url = "jdbc:mysql://localhost:3306/?useSSL=false";
+        String url = "jdbc:mysql://localhost:3306";
         String username = "root";
         String password = "Hickman21!";
 
-        try {
-         //  Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(url, username, password);
-            System.out.println("Connected to the database");
-            // Do something with the connection...
-            Statement stmt = conn.createStatement();
-            String prompt = "SELECT * FROM home_library.book JOIN home_library.author ON book.Author_id = author.Author_id WHERE author.Gender = ";
-            prompt = prompt.concat(gender);
-            prompt = prompt.concat(";");
-            ResultSet rs = stmt.executeQuery(prompt);
-            conn.close();
-        } catch (SQLException e) {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM home_library.Book JOIN home_library.Author ON Book.Author_id = Author.Author_id WHERE Author.Gender = ?")) {
+            stmt.setString(1, gender);
+            ResultSet rs = stmt.executeQuery();
+            boolean found = false;
+
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            while (rs.next()) {
+                found = true;
+                // print all columns dynamically
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(metaData.getColumnName(i) + ": " + rs.getString(i) + " ");
+                }
+                System.out.println();
+            }
+            if (!found) {
+                System.out.println("No authors found with the gender: " + gender);
+            }
+        } catch (SQLException | ClassNotFoundException e) {
             System.out.println("Unable to connect to the database: " + e.getMessage());
         }
-
     }
+
 
     public static void searchByYearPublished(String yearPub) throws SQLException {
 
