8ab133fc0ac86f7e0a36fc1bd7e90a67a2d730fb
Andrew <andrew.klusmeyer@gmail.com>
2023-05-05 23:38:26 -0500

    andrews help

--- src/main/java/com/example/databaseproject3/Functions_for_Find_Book_Info.java
+++ src/main/java/com/example/databaseproject3/Functions_for_Find_Book_Info.java
@@ -172,97 +172,131 @@ public class Functions_for_Find_Book_Info {
 
 
     public static void searchByYearPublished(String yearPub) throws SQLException {
-
-        String url = "jdbc:mysql://localhost:3306/?useSSL=false";
+        String url = "jdbc:mysql://localhost:3306";
         String username = "root";
         String password = "Hickman21!";
 
-        try {
-          //  Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(url, username, password);
-            System.out.println("Connected to the database");
-            // Do something with the connection...
-            Statement stmt = conn.createStatement();
-            String prompt = "SELECT * FROM home_library.book WHERE book.Year_Published = ";
-            prompt = prompt.concat(yearPub);
-            prompt = prompt.concat(";");
-            ResultSet rs = stmt.executeQuery(prompt);
-            conn.close();
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM home_library.Book WHERE Year_Published = ?")) {
+            stmt.setString(1, yearPub);
+            ResultSet rs = stmt.executeQuery();
+            boolean found = false;
+
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            while (rs.next()) {
+                found = true;
+                // print all columns dynamically
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(metaData.getColumnName(i) + ": " + rs.getString(i) + " ");
+                }
+                System.out.println();
+            }
+            if (!found) {
+                System.out.println("No books found published in the year: " + yearPub);
+            }
         } catch (SQLException e) {
             System.out.println("Unable to connect to the database: " + e.getMessage());
         }
-
     }
+
 
 
     public static void searchByPageNum(String numPages) throws SQLException {
-
-        String url = "jdbc:mysql://localhost:3306/?useSSL=false";
+        String url = "jdbc:mysql://localhost:3306";
         String username = "root";
         String password = "Hickman21!";
 
-        try {
-        //    Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(url, username, password);
-            System.out.println("Connected to the database");
-            // Do something with the connection...
-            Statement stmt = conn.createStatement();
-            String prompt = "SELECT * FROM home_library.book WHERE book.Num_of_Pages = ";
-            prompt = prompt.concat(numPages);
-            prompt = prompt.concat(";");
-            ResultSet rs = stmt.executeQuery(prompt);
-            conn.close();
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM home_library.Book WHERE Num_of_Pages = ?")) {
+            stmt.setString(1, numPages);
+            ResultSet rs = stmt.executeQuery();
+            boolean found = false;
+
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            while (rs.next()) {
+                found = true;
+                // print all columns dynamically
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(metaData.getColumnName(i) + ": " + rs.getString(i) + " ");
+                }
+                System.out.println();
+            }
+            if (!found) {
+                System.out.println("No books found with " + numPages + " pages");
+            }
         } catch (SQLException e) {
             System.out.println("Unable to connect to the database: " + e.getMessage());
         }
-
     }
+
+
 
 
     public static void searchByGenre(String genre) throws SQLException {
-
-        String url = "jdbc:mysql://localhost:3306/?useSSL=false";
+        String url = "jdbc:mysql://localhost:3306";
         String username = "root";
         String password = "Hickman21!";
 
-        try {
-          //  Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(url, username, password);
-            System.out.println("Connected to the database");
-            // Do something with the connection...
-            Statement stmt = conn.createStatement();
-            String prompt = "SELECT * FROM home_library.book JOIN home_library.genre ON book.Genre = genre.Genre_id WHERE genre.Genre_Name = ";
-            prompt = prompt.concat(genre);
-            prompt = prompt.concat(";");
-            ResultSet rs = stmt.executeQuery(prompt);
-            conn.close();
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM home_library.book JOIN home_library.genre ON book.Genre = genre.Genre_id WHERE genre.Genre_Name = ?")) {
+            stmt.setString(1, genre);
+            ResultSet rs = stmt.executeQuery();
+            boolean found = false;
+
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            while (rs.next()) {
+                found = true;
+                // print all columns dynamically
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(metaData.getColumnName(i) + ": " + rs.getString(i) + " ");
+                }
+                System.out.println();
+            }
+            if (!found) {
+                System.out.println("No books found with genre: " + genre);
+            }
         } catch (SQLException e) {
             System.out.println("Unable to connect to the database: " + e.getMessage());
         }
-
     }
 
-    public static void searchByBookType(String type) throws SQLException {
 
-        String url = "jdbc:mysql://localhost:3306/?useSSL=false";
+    public static void searchByBookType(String type) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306";
         String username = "root";
         String password = "Hickman21!";
 
-        try {
-         //   Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(url, username, password);
-            System.out.println("Connected to the database");
-            // Do something with the connection...
-            Statement stmt = conn.createStatement();
-            String prompt = "SELECT * FROM home_library.book WHERE book.Book_Type = ";
-            prompt = prompt.concat(type);
-            prompt = prompt.concat(";");
-            ResultSet rs = stmt.executeQuery(prompt);
-            conn.close();
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM home_library.Book WHERE Book_Type = ?")) {
+            stmt.setString(1, type);
+            ResultSet rs = stmt.executeQuery();
+            boolean found = false;
+
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            while (rs.next()) {
+                found = true;
+                // print all columns dynamically
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.print(metaData.getColumnName(i) + ": " + rs.getString(i) + " ");
+                }
+                System.out.println();
+            }
+            if (!found) {
+                System.out.println("No books found with type: " + type);
+            }
         } catch (SQLException e) {
             System.out.println("Unable to connect to the database: " + e.getMessage());
         }
-
     }
 
 
